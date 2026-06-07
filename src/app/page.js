8d9212fd8330d22c5203eda5c0dce2585be5767a
Ifeanyi Chima <chimaifeanyi29@gmail.com>
2026-06-07 20:13:26 +0100

    feat: add design system in global css

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,5 @@
-import Image from "next/image";
+import { redirect } from 'next/navigation';
 
 export default function Home() {
-  return (
-    <main>
-      <h1>Solstice Cipher</h1>
-    </main>
-  );
+  return redirect('/home');
 }
